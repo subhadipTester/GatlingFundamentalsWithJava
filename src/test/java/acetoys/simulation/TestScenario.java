d631029d0ccd40ae82c7371903a4756a0c557503
Subhadip Roy <subhadipsingh.referral@gmail.com>
2023-01-25 20:07:49 +0530

    This is another tweak to TestPopulation and TestScenario Classes

--- src/test/java/acetoys/simulation/TestScenario.java
+++ src/test/java/acetoys/simulation/TestScenario.java
@@ -1,51 +1,51 @@
 package acetoys.simulation;
+
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
+
+import java.time.Duration;
+
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
+
 public class TestScenario {
+    private static final Duration TEST_DURATION = Duration.ofSeconds(Integer.parseInt(System.getenv("DURATION")));
 
-public static ScenarioBuilder defaultLoadTest =
-
-        scenario("Default Load TestSuite")
-        .during(60)
-        .on(
-
-                randomSwitch().on(
-
-                  Choice.withWeight(60, exec(UserJourney.browseStore)),
-                  Choice.withWeight(30, exec(UserJourney.completePurchase)),
-                  Choice.withWeight(10,exec(UserJourney.abandonBasket))
-
-                )
+    public static ScenarioBuilder defaultLoadTest =
 
 
+            scenario("Default Load TestSuite")
+                    .during(TEST_DURATION)
+                    .on(
+
+                            randomSwitch().on(
+
+                                    Choice.withWeight(60, exec(UserJourney.browseStore)),
+                                    Choice.withWeight(30, exec(UserJourney.completePurchase)),
+                                    Choice.withWeight(10, exec(UserJourney.abandonBasket))
+
+                            )
 
 
-        );
+                    );
 
-public static ScenarioBuilder highPurchaseLoadTest =
+    public static ScenarioBuilder highPurchaseLoadTest =
 
-        scenario("High Volume Load TestSuite")
-        .during(180)
-        .on(
+            scenario("High Volume Load TestSuite")
+                    .during(TEST_DURATION)
+                    .on(
 
-                randomSwitch().on(
+                            randomSwitch().on(
 
-                 Choice.withWeight(30, exec(UserJourney.browseStore)),
-                 Choice.withWeight(40, exec(UserJourney.completePurchase)),
-                 Choice.withWeight(30,exec(UserJourney.abandonBasket))
-
-
-
-                )
+                                    Choice.withWeight(30, exec(UserJourney.browseStore)),
+                                    Choice.withWeight(40, exec(UserJourney.completePurchase)),
+                                    Choice.withWeight(30, exec(UserJourney.abandonBasket))
 
 
+                            )
 
 
-        );
-
-
+                    );
 
 
 }
